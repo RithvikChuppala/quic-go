b6c17602f19317cc2945a1addd4158643bf67f6a
Rithvik Chuppala <rithv@MacBook-Pro.local>
2023-11-19 15:21:30 -0800

    add back println

--- connection.go
+++ connection.go
@@ -2255,6 +2255,7 @@ func (s *connection) AcceptUniStream(ctx context.Context) (ReceiveStream, error)
 
 // OpenStream opens a stream
 func (s *connection) OpenStream() (Stream, error) {
+	fmt.Println("ILLAYAYELLEI")
 	return s.streamsMap.OpenStream()
 }
 
